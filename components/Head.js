8081407f6f98af7601626e75b783a23f613570fd
Diego Bernal <diego91bernal@gmail.com>
2020-01-20 08:40:23 -0800

    update

--- components/Head.js
+++ components/Head.js
@@ -43,6 +43,21 @@ const Head = ({ description, title, url }) => (
     `
       }}
     ></script>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=AW-679239646"
+    ></script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'AW-679239646');
+    `
+      }}
+    ></script>
   </NextHead>
 );
 
