0b6f20dde61b110fe94cafac66b8e43e2b7517f9
Diego Bernal <diego91bernal@gmail.com>
2023-08-23 16:58:46 -0700

    Add gtm script

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ import { ServerStyleSheet } from "styled-components";
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
     );
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
@@ -14,8 +14,27 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PT864XJ5');`
+            }}
+          />
+          {this.props.styleTags}
+        </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PT864XJ5"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `
+            }}
+          />
           <Main />
           <NextScript />
           <script
