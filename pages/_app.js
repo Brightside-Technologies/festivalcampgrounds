e684c6b266192cc4b48020e2133f08aa8ef7acc7
Diego Bernal <diego91bernal@gmail.com>
2024-01-11 16:36:22 -0800

    Add gtm scripts again

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from "next/app";
+import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -6,6 +7,26 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 export default class Site extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-58JM20L3GC"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-58JM20L3GC');
+              `
+            }}
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
   }
 }
